20eb0e7083dc352c7fc29e41c74b100389cb8c96
sumitrajawat <rajawatsumit579@gmail.com>
2026-09-21 18:16:25 +0700

    Add 03_DATATYPES in JAVA+DSA

---
+++ 03_DATATYPES/integer.java
@@ -0,0 +1,13 @@
+public class integer {
+    public static void main(String[] args){
+        int age = 30; // here int is a data type of the category of integer
+        float point = 9.0f;
+        double large = 73.8789327274;
+        System.out.println(age);
+        System.out.println(point);
+        System.err.println(large);
+
+    }
+    
+
+}
