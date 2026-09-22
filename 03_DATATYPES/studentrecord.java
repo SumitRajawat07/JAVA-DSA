9e9675d4c487aee1c3251f16a32243512fc58dcb
sumitrajawat <rajawatsumit579@gmail.com>
2026-09-22 18:00:40 +0700

    Add student record in JAVA+DSA

---
+++ 03_DATATYPES/studentrecord.java
@@ -0,0 +1,13 @@
+public class studentrecord {
+    public static void main(String[] args) {
+        
+        int age = 20 ;
+        float percentage = 78.8f;
+        char grade = 'A';
+
+        System.out.println(age);
+        System.out.println(percentage);
+        System.out.println(grade);
+
+    }
+}
