e573c0cea86121b453bfd4d97934b52ff518b690
sumitrajawat <rajawatsumit579@gmail.com>
2026-09-24 22:16:01 +0700

    Add assignment in operator

---
+++ 05_OPRATORS/assignment.java
@@ -0,0 +1,12 @@
+public class assignment {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 3;
+
+         a += 9;  // here a assign the value with the help of equal so the equal is a assignment operator
+         b += 20;
+        System.out.println(a);
+        System.out.println(b);
+    }
+    
+}
