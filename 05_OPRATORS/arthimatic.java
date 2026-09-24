7fc4bb03f322ed31de7a76ede001468da40e2581
sumitrajawat <rajawatsumit579@gmail.com>
2026-09-24 22:09:05 +0700

    Add arthimatic in operators

---
+++ 05_OPRATORS/arthimatic.java
@@ -0,0 +1,13 @@
+public class arthimatic {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 9;
+
+        System.out.println("the sum of a and b is  : " + (a+b)); // here addition is a operator and a and b is operands
+        System.out.println("the subtract of a and b is  : " + (a-b)); // here subtract is a operator and a and b is operands
+        System.out.println("the multiply of a and b is  : " + (a*b)); // here multiplication is a operator and a and b is operands
+        System.out.println("the divide of a and b is  : " + (a/b)); // here division is a operator and a and b is operands
+        System.out.println("the modulas of a and b is  : " + (a%b)); // here modulas is a operator and a and b is operands
+    }
+    
+}
