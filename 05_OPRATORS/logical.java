a6d3a47605982db057b28c7701b8b45bf7fd09f1
sumitrajawat <rajawatsumit579@gmail.com>
2026-09-25 14:41:22 +0700

    Add logical in operators

---
+++ 05_OPRATORS/logical.java
@@ -0,0 +1,12 @@
+public class logical {
+   public static void main(String[] args) {
+      int a = 4;
+      int b = 54;
+      // here && , || , ! is a logical operator 
+      System.out.println(a > b && b < a);
+      System.out.println(a > b || b > a);
+      System.out.println(a > b != b < a);
+   }   
+}
+    
+
