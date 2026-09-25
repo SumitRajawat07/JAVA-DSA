25518aa87adb2b3b36c3874d03017bbddf40cba9
sumitrajawat <rajawatsumit579@gmail.com>
2026-09-25 14:49:19 +0700

    Add comparison in operators

---
+++ 05_OPRATORS/comparison.java
@@ -0,0 +1,14 @@
+public class comparison {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a==b);
+
+        // here  > , < , >= , <= , == is comparison operator
+    }
+}
