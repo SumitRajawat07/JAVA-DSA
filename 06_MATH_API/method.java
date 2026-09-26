43132e095cc6bdbfaeefc6918670017688abded2
sumitrajawat <rajawatsumit579@gmail.com>
2026-09-26 11:48:53 +0700

    Add method in MATH_API

---
+++ 06_MATH_API/method.java
@@ -0,0 +1,15 @@
+public class method {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-9));
+        System.out.println(Math.max(354, 423));
+        System.out.println(Math.min(354, 423));
+        System.out.println(Math.sqrt(354));
+        System.out.println(Math.round(3.6));
+        System.out.println(Math.pow(4, 2));
+        System.out.println(Math.ceil(3.3));
+        System.out.println(Math.floor(17.8));
+        System.out.println(Math.random());
+        
+    }
+    
+}
