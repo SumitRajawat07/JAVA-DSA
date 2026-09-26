8f246ce7e906584a5a7c00fc73fdd7b7ea506a57
sumitrajawat <rajawatsumit579@gmail.com>
2026-09-26 11:57:35 +0700

    Add area_triangle in MATH_API

---
+++ 06_MATH_API/area_triangle.java
@@ -0,0 +1,12 @@
+public class area_triangle {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+
+        float area = 0.5f * a * b ;
+
+        System.err.println(area);
+
+    }
+    
+}
